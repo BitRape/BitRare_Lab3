f22cbc68fd430e70805a09365b2608b7aed2000b
NILIDA <107945282+NILIDA@users.noreply.github.com>
2024-12-11 14:01:16 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -21,25 +21,23 @@ TEST(PassPuckTest, OpponentTeamPass) {
     EXPECT_NE(game.getCurrentHolder() / 6, 0);
 }
 
-TEST(PassPuckTest, SuccessfulPassesIncrement) {
-    std::srand(0);
+TEST(ScoreTest, UpdateScore) {
     HockeyGame game(0, 12);
-    game.setCurrentHolder(2); // Player from team 0
-    game.passPuck();
 
-    EXPECT_EQ(game.getSuccessfulPasses(), 1);
+    // Убедимся, что currentHolder принадлежит команде 1
+    game.setCurrentHolder(0);
+    game.setSuccessfulPasses(SUCCESSFUL_PASSES_FOR_GOAL);
+    game.passPuck();
+    EXPECT_EQ(game.getTeamScore(0), 1);
 }
 
-TEST(PassPuckTest, SuccessfulPassesResetOnOpponentPass) {
-    std::srand(0);
+TEST(GameTest, PlayGameSingleProcess) {
     HockeyGame game(0, 12);
-    game.setCurrentHolder(2); // Player from team 0
-    game.setSuccessfulPasses(2);
-    game.passPuck();
+    game.playGame();
 
-    EXPECT_EQ(game.getSuccessfulPasses(), 0);
+    EXPECT_GE(game.getTeamScore(0), 0);
+    EXPECT_GE(game.getTeamScore(1), 0);
 }
-
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
