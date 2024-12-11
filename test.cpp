2d3e1c41e03eb8641910a9919a08c770b4cf4581
NILIDA <107945282+NILIDA@users.noreply.github.com>
2024-12-11 12:55:20 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,40 +1,46 @@
 #define TESTING
 #include <gtest/gtest.h>
 #include <mpi.h>
-#include "hockeyGame.cpp"
+#include "hockeyGame.сpp" // Убедитесь, что этот файл существует и содержит объявление класса HockeyGame
 
+// Мок функций для проверки передачи шайбы
 TEST(PassPuckTest, SameTeamPass) {
-    std::srand(0);
+    std::srand(0); // Зафиксировать генерацию случайных чисел для всех процессов
     HockeyGame game(0, 12);
-    game.setCurrentHolder(2);
+    game.currentHolder = 2; // Игрок 2 из первой команды
     game.passPuck();
 
-    EXPECT_NE(game.getCurrentHolder(), 2);
+    // Важно: шайба должна быть передана другому игроку (внутри одной команды)
+    EXPECT_NE(game.currentHolder, 2);
 }
 
 TEST(PassPuckTest, OpponentTeamPass) {
-    std::srand(0);
+    std::srand(0); // Зафиксировать генерацию случайных чисел
     HockeyGame game(0, 12);
-    game.setCurrentHolder(2);
+    game.currentHolder = 2; // Игрок 2 из первой команды
     game.passPuck();
 
-    EXPECT_NE(game.getCurrentHolder() / 6, 0);
+    // Проверяем, что шайба передана противнику
+    EXPECT_NE(game.currentHolder / 6, 0);  // Игрок из другой команды
 }
 
 TEST(ScoreTest, UpdateScore) {
     HockeyGame game(0, 12);
 
-    game.setSuccessfulPasses(SUCCESSFUL_PASSES_FOR_GOAL);
+    // После успешного гола для команды 0
+    game.successfulPasses = SUCCESSFUL_PASSES_FOR_GOAL;
     game.passPuck();
-    EXPECT_EQ(game.getTeamScore(0), 1);
+
+    EXPECT_EQ(game.teamScores[0], 1); // Команда 0 должна получить 1 гол
 }
 
 TEST(GameTest, PlayGameSingleProcess) {
     HockeyGame game(0, 12);
     game.playGame();
 
-    EXPECT_GE(game.getTeamScore(0), 0);
-    EXPECT_GE(game.getTeamScore(1), 0);
+    // Можно протестировать состояние после игры
+    EXPECT_GE(game.teamScores[0], 0);
+    EXPECT_GE(game.teamScores[1], 0);
 }
 
 int main(int argc, char **argv) {
