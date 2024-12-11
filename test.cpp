473ad6f3615ceb22971ca550e571964b43d1632b
NILIDA <107945282+NILIDA@users.noreply.github.com>
2024-12-11 12:48:47 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,35 +5,35 @@
 TEST(PassPuckTest, SameTeamPass) {
     std::srand(0);
     HockeyGame game(0, 12);
-    game.currentHolder = 2;
+    game.setCurrentHolder(2);
     game.passPuck();
 
-    EXPECT_NE(game.currentHolder, 2);
+    EXPECT_NE(game.getCurrentHolder(), 2);
 }
 
 TEST(PassPuckTest, OpponentTeamPass) {
     std::srand(0);
     HockeyGame game(0, 12);
-    game.currentHolder = 2;
+    game.setCurrentHolder(2);
     game.passPuck();
 
-    EXPECT_NE(game.currentHolder / 6, 0);
+    EXPECT_NE(game.getCurrentHolder() / 6, 0);
 }
 
 TEST(ScoreTest, UpdateScore) {
     HockeyGame game(0, 12);
 
-    game.successfulPasses = SUCCESSFUL_PASSES_FOR_GOAL;
+    game.setSuccessfulPasses(SUCCESSFUL_PASSES_FOR_GOAL);
     game.passPuck();
-    EXPECT_EQ(game.teamScores[0], 1);
+    EXPECT_EQ(game.getTeamScore(0), 1);
 }
 
 TEST(GameTest, PlayGameSingleProcess) {
     HockeyGame game(0, 12);
     game.playGame();
 
-    EXPECT_GE(game.teamScores[0], 0);
-    EXPECT_GE(game.teamScores[1], 0);
+    EXPECT_GE(game.getTeamScore(0), 0);
+    EXPECT_GE(game.getTeamScore(1), 0);
 }
 
 int main(int argc, char **argv) {
